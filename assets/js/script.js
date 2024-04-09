a6e26f4f18fbb61cf85b3b14962a88e2d45e4fea
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-09 14:19:51 +0530

    added back to top btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,3 +78,17 @@ $('.slider').slick({
     // instead of a settings object
   ]
 });
+
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show"); 
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >100) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
