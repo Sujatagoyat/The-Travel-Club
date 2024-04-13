304e01ad65c4ed1bb2c347cf7e0d55164317f841
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-13 11:08:27 +0530

    updated cross icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const menuIcon = document.querySelector(".menuIcon")
 
 menuIcon.addEventListener("click", function () {
   menulist.classList.toggle("show");
+  menuIcon.classList.toggle("show");
   document.body.classList.toggle("overFlowHidden")
   document.querySelector(".menuIcon").classList.toggle("cross")
 })
